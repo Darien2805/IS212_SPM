5d2f84f471155874cd21d22fb1ac1af03b64c8ab
hweezz <tsehwee2@gmail.com>
2022-09-29 21:03:13 +0800

    add database connection (midway)

--- frontend/src/pages/Courses.js
+++ frontend/src/pages/Courses.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Axios from 'axios'
+// import React from 'react'
 import Header from '../components/Header'
 function Courses() {
+  const [courses,setCourses]=useState([]);
+
+  useEffect(()=>{
+    Axios.get("http://localhost:5000/api/getCourses").then((data)=>{
+      setCourses(data.data)
+    });
+    },[])
+    
   return (
     <>
     <Header />
     <div>This is courses</div>
+    {courses}
+    {/* {courses.map((val,key)=>{
+      return (
+        <div>
+          <table>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Desc</th>
+              <th>Status</th>
+              <th>Type</th>
+            </tr>
+            {courses.map((val,key)=>{
+              return(
+                <tr>
+                  <td>{val.course_id}</td>
+                  <td>{val.course_name}</td>
+                  <td>{val.course_desc}</td>
+                  <td>{val.course_status}</td>
+                  <td>{val.course_type}</td>
+                </tr>
+              )
+            })}
+          </table>
+        </div>
+    )  })}   */}
     </>
   )
 }
