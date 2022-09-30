294be6f93a2af4f1c7d083da6d60e08fcb50117e
Potatomochi <noahrsam@gmail.com>
2022-09-30 14:47:59 +0800

    added gitignore

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,4 @@
 export {default as Home} from "./Home";
 export {default as Roles} from "./Roles";
 export {default as Courses} from "./Courses";
-export {default as EasterEgg} from "./EasterEgg";
+export {default as EasterEgg} from "./EasterEgg.js";
