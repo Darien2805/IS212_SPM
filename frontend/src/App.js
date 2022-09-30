5aab0a52b3a2fe110a4a3ef1b31a3dc92b56bff8
Potatomochi <noahrsam@gmail.com>
2022-09-30 14:25:03 +0800

    Made an easter egg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import {BrowserRouter,Route,Routes} from "react-router-dom"
-import {Home, Roles,Courses} from "./pages"
+import {Home, Roles,Courses,EasterEgg} from "./pages"
 import * as ROUTES from './constants/routes.js';
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <Route path={ROUTES.HOME} element={<Home />} exact></Route>
       <Route path={ROUTES.ROLES} element={<Roles />}></Route>
       <Route path={ROUTES.COURSES} element={<Courses />}></Route>
+      <Route path={ROUTES.EASTEREGG} element={<EasterEgg />}></Route>
       </Routes>
 
     </BrowserRouter>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,4 @@
 export {default as Home} from "./Home";
 export {default as Roles} from "./Roles";
 export {default as Courses} from "./Courses";
+export {default as EasterEgg} from "./EasterEgg";
