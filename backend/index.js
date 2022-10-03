514dad31bf6ed023019561597a8c67c26823aad8
hweezz <tsehwee2@gmail.com>
2022-10-03 13:47:21 +0800

    add route for learning journey courses

--- backend/index.js
+++ backend/index.js
@@ -277,7 +277,69 @@ app.get("/api/getJourneys/:staff_id", (req,res)=>{
     });
 });
 
+// Route to get journey courses for selected journey
+app.get("/api/getJourneyCourses/:journey_id", (req,res)=>{
+    const journey_id = req.params.journey_id;
+    const stmt = `SELECT skills.skill_id as role_skill_id, course_id, course_name, course_desc, course_status, course_type, courses.skill_id as course_skill_id FROM (
+                    SELECT skill_id FROM roleskill WHERE role_id = 
+                        (SELECT role_id FROM journey WHERE journey_id = 8)
+                    ) AS skills
+                LEFT JOIN (
+                    SELECT c.course_id, course_name, course_desc, course_status, course_type, skill_id FROM courseskill cs, journeycourse jc, course c
+                        WHERE cs.course_id = jc.course_id AND c.course_id = jc.course_id AND journey_id = ?
+                    ) as courses
+                ON skills.skill_id=courses.skill_id
+                UNION ALL
+                SELECT skills.skill_id as role_skill_id, course_id, course_name, course_desc, course_status, course_type, courses.skill_id as course_skill_id FROM (
+                    SELECT skill_id FROM roleskill WHERE role_id = 
+                        (SELECT role_id FROM journey WHERE journey_id = 8)
+                    ) AS skills
+                RIGHT JOIN (
+                    SELECT c.course_id, course_name, course_desc, course_status, course_type, skill_id FROM courseskill cs, journeycourse jc, course c
+                        WHERE cs.course_id = jc.course_id AND c.course_id = jc.course_id AND journey_id = ?
+                    ) as courses
+                ON skills.skill_id=courses.skill_id`
+    db.query(stmt, [journey_id, journey_id], (err,result)=>{
+        if(err) {
+            console.log(err)
+        }
+        res.send(result)
+    });
+});
 
+// // Route to get courses related to skill that are added for selected journey
+// app.get("/api/getJourneySkillCourses/:journey_id/:skill_id", (req,res)=>{
+//     const journey_id = req.params.journey_id;
+//     const skill_id = req.params.skill_id;
+//     const stmt = `SELECT * FROM course WHERE course_id in (
+//                         SELECT course_id FROM courseskill WHERE skill_id = ?
+//                     ) AND course_id in (
+//                         SELECT course_id FROM journeycourse WHERE journey_id = ?
+//                     )`
+//     db.query(stmt, [skill_id, journey_id], (err,result)=>{
+//         if(err) {
+//             console.log(err)
+//         }
+//         res.send(result)
+//     });
+// });
+
+// // Route to get other courses that are added for selected journey (not related to the skills of the role)
+// app.get("/api/getJourneyOtherCourses/:journey_id", (req,res)=>{
+//     const journey_id = req.params.journey_id;
+//     const skill_id = req.params.skill_id;
+//     const stmt = `SELECT * FROM course WHERE course_id in (
+//                         SELECT course_id FROM courseskill WHERE skill_id = ?
+//                     ) AND course_id in (
+//                         SELECT course_id FROM journeycourse WHERE journey_id = ?
+//                     )`
+//     db.query(stmt, [skill_id, journey_id], (err,result)=>{
+//         if(err) {
+//             console.log(err)
+//         }
+//         res.send(result)
+//     });
+// });
 
 
 
