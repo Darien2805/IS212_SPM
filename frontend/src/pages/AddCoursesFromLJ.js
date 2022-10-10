68f9044d62cd5097cec3189fd6ffdd9571c820c0
Potatomochi <noahrsam@gmail.com>
2022-10-10 13:42:53 +0800

    Initial Branch Commit

---
+++ frontend/src/pages/AddCoursesFromLJ.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+
+function AddCoursesFromLJ() {
+    const [selectSkills,setSelectSkills] = useState([])
+    
+  return (
+
+    <div>addCoursesFromLJ</div>
+    
+  )
+}
+
+export default AddCoursesFromLJ
