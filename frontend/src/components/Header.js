e429b5c529f87621dee4ff1a20b02941f261063b
Darien <63910112+Darien2805@users.noreply.github.com>
2022-10-12 13:38:30 +0800

    edited header files
    Resolved minor css issues with headers

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,42 +8,42 @@ import Axios from 'axios'
 
 function Header() {
 
-    const [staffid, setStaffid] = useState('');
-    const [staffType, setStaffType] = useState('');
-    const [staffName, setStaffName] = useState('');
-    //const userIsStaff = true
-    const sessionId = window.localStorage.getItem('sessionId');
-    useEffect(()=>{
-      if (sessionId != null){
-        console.log(sessionId)
-        Axios.get("http://localhost:5005/api/getUserType/"+sessionId).then((response)=>{
-          if (response != ''){
-            console.log(response.data)
-            setStaffName(response.data[0].staff_fname)
-            //console.log(response.data[0].staff_fname)
-            setStaffType(response.data[0].user_type)
-            //console.log(response.data[0].user_type)
-          }
-        });
-      }
-    },[])
-
-    const logout = (e) => {
-      window.localStorage.removeItem('sessionId');
+  const [staffid, setStaffid] = useState('');
+  const [staffType, setStaffType] = useState('');
+  const [staffName, setStaffName] = useState('');
+  //const userIsStaff = true
+  const sessionId = window.localStorage.getItem('sessionId');
+  useEffect(()=>{
+    if (sessionId != null){
+      //console.log(sessionId)
+      Axios.get("http://localhost:5005/api/getUserType/"+sessionId).then((response)=>{
+        if (response != ''){
+          //console.log(response.data)
+          setStaffName(response.data[0].staff_fname)
+          //console.log(response.data[0].staff_fname)
+          setStaffType(response.data[0].user_type)
+          //console.log(response.data[0].user_type)
+        }
+      });
     }
-    //console.log(sessionId)
-    //1 - Admin
-    //2 - User
-    //3 - Manager
-    //4 - Trainer
+  },[])
 
-    return (
-        <>
-        <div className="headerContainer">
+  const logout = (e) => {
+    window.localStorage.removeItem('sessionId');
+  }
+  //console.log(sessionId)
+  //1 - Admin
+  //2 - User
+  //3 - Manager
+  //4 - Trainer
+
+  return (
+      <>
+      <div className="headerContainer">
         <div className="brandingLeft">
-            <div>
-                <p>Logo</p>
-            </div>
+          <div>
+              <p>Logo</p>
+          </div>
         </div>
         {/* <div className="searchBar">
           <Route
@@ -52,7 +52,7 @@ function Header() {
             )}
           ></Route>
         </div> */}
-        
+      
 
         <div className="headerRightContainer">
         {staffType == '1'? (
@@ -62,8 +62,7 @@ function Header() {
             <Link to="/courses" className="headerLink">Courses</Link>
             <div className="dropdown">
             
-
-            <Link to="/teams" className="headerLink">Systems</Link>
+            <Link className="headerLink">Systems</Link>
             <div className="dropdown-content">
                     <Link to="/" className="dropdownLink">Role Maintenance</Link>
                     <Link to="/SkillAssignment" className="dropdownLink">Skill Assignment</Link>
@@ -72,7 +71,7 @@ function Header() {
             <button className="dropbtn"><KeyboardArrowDownIcon  /></button>
             <Link to="/" className="headerLink" onClick={e=>logout(e)}>Log Out</Link>
             </div>
-      
+            {staffName}
         </div>
         ) : ( staffType == '3' ?
             <div className="nav-links">
@@ -81,7 +80,7 @@ function Header() {
               <Link to="/courses" className="headerLink">Courses</Link>
               <Link to="/teams" className="headerLink">Teams</Link>
               <Link to="/" className="headerLink" onClick={e=>logout(e)}>Log Out</Link>
-
+              {staffName}
             </div> : 
 
             <div className="nav-links">
@@ -90,20 +89,14 @@ function Header() {
             <Link to="/roles" className="headerLink">Roles</Link>
             <Link to="/courses" className="headerLink">Courses</Link>
             <Link to="/" className="headerLink" onClick={e=>logout(e)}>Log Out</Link>
-
-          </div>
-        ) 
-    
-    }
-    <div>
-        <p>{staffName}</p>
-        
-    </div>
+            {staffName}
+            </div>
+      )}
         </div>
       </div>
-    
-      </>
-    )
+  
+    </>
+  )
 }
 
 export default Header
