5ea041b713fe43cd07f2030938683b7174b8045e
Potatomochi <noahrsam@gmail.com>
2022-10-14 13:55:44 +0800

    Removed the Easter Egg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import {BrowserRouter,Route,Routes} from "react-router-dom"
-import {Home, Roles, Courses, SkillMaintenance, SkillAssignment, CreateSkill,EasterEgg, LearningJourney, RoleMaintenance, CreateRole} from "./pages"
+import {Home, Roles, Courses, SkillMaintenance, SkillAssignment, CreateSkill, LearningJourney, RoleMaintenance, CreateRole} from "./pages"
 import * as ROUTES from './constants/routes.js';
 
 function App() {
@@ -14,7 +14,6 @@ function App() {
       <Route path={ROUTES.SKILLASSIGNMENT} element={<SkillAssignment />}></Route>
       <Route path={ROUTES.SKILLMAINTENANCE} element={<SkillMaintenance />}></Route>
       <Route path={ROUTES.CREATESKILL} element={<CreateSkill />}></Route>
-      <Route path={ROUTES.EASTEREGG} element={<EasterEgg />}></Route>
       <Route path={ROUTES.LEARNINGJOURNEY} element={<LearningJourney />}></Route>
       <Route path={ROUTES.ROLEMAINTENANCE} element={<RoleMaintenance />}></Route>
       <Route path={ROUTES.CREATEROLE} element={<CreateRole />}></Route>
