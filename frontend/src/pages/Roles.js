4dc9c4643a1f856419de6b15f5fd64eec0dbdb50
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2022-10-15 18:51:16 +0800

    Update Roles.js

--- frontend/src/pages/Roles.js
+++ frontend/src/pages/Roles.js
@@ -1,12 +1,15 @@
 import React,{useState,useEffect} from 'react'
 import Axios from 'axios'
-
 import Header from '../components/Header'
 import RoleCard from '../components/roleCard';
 import "./Roles.css"
+
 //Bootstrap
+import Alert from 'react-bootstrap/Alert';
+
+
 function Role() {
-  const [roles,setRoles] = useState([]);
+  const [activeRoles,setRoles] = useState([]);
   const staff_id = 2
 
   useEffect(()=>{
@@ -22,10 +25,15 @@ function Role() {
       <div >
         <h1>All Roles</h1>
         <div className="child">
-          {roles.map((val)=>{
-            return( val.role_status === "Active" ?
-            <RoleCard key={val.role_id} roleName={val.role_name} roleDesc={val.role_desc} skillNames={val.skill_names}/> : [])
-            })}
+          {activeRoles.map((role)=>{
+            return( role.role_status === "Active" ?
+            <RoleCard key={role.role_id} roleName={role.role_name} roleDesc={role.role_desc} skillNames={JSON.parse(role.skill_names)}
+              journey_ID={role.journey_id}/> 
+            :
+            <Alert className='alert' key='warning' variant='warning'>
+              No active job roles at the moment
+            </Alert>)
+          })}
         </div>
       </div>
     </div>
