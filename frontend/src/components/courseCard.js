f499f7ccd2aa68e0d7d0b675ac39428476614378
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2022-10-19 03:08:08 +0800

    Update courseCard.js

--- frontend/src/components/courseCard.js
+++ frontend/src/components/courseCard.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import "./courseCard.css";
+import { useNavigate } from "react-router-dom";
 
-function CourseCard({courseName,courseDesc}) {
+//bootstrap
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+
+function CourseCard({courseName,courseDesc,skillNames,courseType}) {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `../Roles`; //need to change the route 
+    navigate(path);
+  }
+
   return (
     <div className="courseCard">
-        <h1>{courseName}</h1>
-        <p>{courseDesc}</p>
-        {/* subject to change , pending th completion of skeel table */}
-        {/* {skills.map((skill) =>(
-            <div key={skill._id}>
-                <p>{skill.name}</p>
-            </div>
-        ))} */}
-        <div className="stageRight">
-            <button>View</button>
-        </div>
-    </div>
-    
+      <h2>{courseName}</h2>
+      <b>
+        <u>Description</u>
+      </b>
+      <p>{courseDesc}</p>
+      <b>
+        <u>Skills needed</u>
+      </b>
+      <p>
+        {skillNames.map((skill)=>{
+          return( 
+          <Badge bg="secondary" className="me-1"> {skill} </Badge>
+          )
+        })}
+      </p>
+      <b>
+        <u>Course Type</u>
+      </b>
+      <p>
+        <Badge bg="secondary" className="me-1">{courseType}</Badge>
+      </p>
+      <div style={{ display: "flex" }}>
+        <Button variant="primary" style={{ marginLeft: "auto" }} onClick={routeChange}>View</Button>
+      </div>
+  </div>  
   )
 }
-
 export default CourseCard
