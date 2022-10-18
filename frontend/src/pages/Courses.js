318c0428fc44aaf8569b508543eb3d05b52193ab
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2022-10-19 03:08:03 +0800

    Update Courses.js

--- frontend/src/pages/Courses.js
+++ frontend/src/pages/Courses.js
@@ -4,9 +4,12 @@ import Header from '../components/Header'
 import CourseCard from '../components/courseCard';
 import "./Courses.css"
 
+//Bootstrap
+import Alert from 'react-bootstrap/Alert';
+
 function Courses() {
   const [activeCourses,setCourses] = useState([]);
-  const staff_id = 2
+  const staff_id = 2 // Testing purpose
 
   useEffect(()=>{
     Axios.get(`http://localhost:5005/api/getActiveCourses/${staff_id}`).then((response)=>{
@@ -23,8 +26,15 @@ function Courses() {
         <div className="child">
             {activeCourses.map((course)=>{
               return(course.course_status === "Active" ?
-                <CourseCard key={course.course_id} courseName={course.course_name} courseDesc={course.course_desc}/> 
-                : "You suck, Ben"
+                <CourseCard key={course.course_id} courseName={course.course_name} courseDesc={course.course_desc}
+                skillNames={JSON.parse(course.skill_names)} courseType={course.course_type}/> 
+                : 
+                <p>
+                  <Alert className='alert' key='warning' variant='warning'>
+                    No active courses at the moment
+                  </Alert>
+                </p>
+                
               )
             })}
         </div>
@@ -33,5 +43,4 @@ function Courses() {
     </>
   )
 }
-
 export default Courses
