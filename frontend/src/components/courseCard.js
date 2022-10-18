16c588b41624cb8d96db0684a8f2739f27ae45f5
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2022-10-18 18:45:48 +0800

    Update courseCard.js

--- frontend/src/components/courseCard.js
+++ frontend/src/components/courseCard.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import "./courseCard.css";
 
-function CourseCard({title,description}) {
+function CourseCard({courseName,courseDesc}) {
   return (
-    
     <div className="courseCard">
-        <h1>{title}</h1>
-        <p>{description}</p>
+        <h1>{courseName}</h1>
+        <p>{courseDesc}</p>
         {/* subject to change , pending th completion of skeel table */}
         {/* {skills.map((skill) =>(
             <div key={skill._id}>
                 <p>{skill.name}</p>
             </div>
         ))} */}
-        <p>Skeels:</p>
         <div className="stageRight">
             <button>View</button>
         </div>
